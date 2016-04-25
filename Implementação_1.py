207ea9ead3756cf33521a5aabfd628b7187208e8
carletti <trcarletti@gmail.com>
2016-04-25 16:46:15 -0300

    Gráficos do pulmão e do sangue

--- Implementação_1.py
+++ Implementação_1.py
@@ -2,7 +2,7 @@
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 import numpy as np
-
+# Definindo equações
 def EquacoesDiferenciais(q, t):
     a = (95/100)
     g = (5/100)
@@ -11,17 +11,25 @@ def EquacoesDiferenciais(q, t):
     dpdt = -(a*q[0]) - (g*q[0])
     dsdt = (a*q[0]) - (b*q[1]) - (m*q[1])
     return [dpdt,dsdt]
-
+# Definindo valores iniciais
 p0 = 10
-s0 = 10   
+s0 = 0   
 q0 = [p0, s0]
+# Definindo intervalo de tempo
 T = np.arange(0,100,0.0001)
-
-s = odeint(EquacoesDiferenciais, q0, T)
-
-plt.plot(T, s[:, 1])
+# Definindo odeint
+q = odeint(EquacoesDiferenciais, q0, T)
+# Gráfico THCp X Tempo
+plt.plot(T, q[:, 0])
+plt.ylabel('p(t)')
+plt.xlabel('t')
+plt.title(r'Quantidade de THC no pulmão')
+plt.grid(True)
+plt.show()
+# Gráfico THCs X Tempo
+plt.plot(T, q[:, 1])
 plt.ylabel('s(t)')
-plt.xlabel('T')
+plt.xlabel('t')
 plt.title(r'Quantidade de THC no sangue')
 plt.grid(True)
 plt.show()
