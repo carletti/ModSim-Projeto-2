1eb49634891ba6342c0f487108d19adc8d5b832e
carletti <trcarletti@gmail.com>
2016-04-25 16:35:03 -0300

    Implementação com programa funcionando

--- Implementação_1.py
+++ Implementação_1.py
@@ -3,24 +3,25 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 import numpy as np
 
-def EquacoesDiferenciais(p, s, a, g, b, m):
-    dpdt = i- (a*p) - (g*p)
-    dsdt = (a*p) - (b*s) - (m*s)
-    return dsdt
+def EquacoesDiferenciais(q, t):
+    a = (95/100)
+    g = (5/100)
+    b = (35/100)
+    m = (65/100)
+    dpdt = -(a*q[0]) - (g*q[0])
+    dsdt = (a*q[0]) - (b*q[1]) - (m*q[1])
+    return [dpdt,dsdt]
 
-p0 = 
-s0 = 
-t = np.arange(0,100,0.0001)
-a = (95/100)
-g = (5/100)
-b = 
-m =
+p0 = 10
+s0 = 10   
+q0 = [p0, s0]
+T = np.arange(0,100,0.0001)
 
-s = odeint(EquacoesDiferenciais, p0, s0, t, args=(a,g,b,m))
+s = odeint(EquacoesDiferenciais, q0, T)
 
-plt.plot(t,  s)
+plt.plot(T, s[:, 1])
 plt.ylabel('s(t)')
-plt.xlabel('t')
+plt.xlabel('T')
 plt.title(r'Quantidade de THC no sangue')
 plt.grid(True)
-plt.show
+plt.show()
