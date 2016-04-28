c4c55f1b27f9d3a7deab44c5a6b23112266f053e
carletti <trcarletti@gmail.com>
2016-04-28 06:21:36 -0300

    Testes

--- Implementação_1.py
+++ Implementação_1.py
@@ -10,26 +10,28 @@ def EquacoesDiferenciais(q, t):
     kevmax = 0.9
     
     dpdt = -(ka*q[0]) - (ks*q[0])
-    dsdt = (ka*q[0]) - (kmvmax*q[1]) 
+    dsdt = (ka*q[0]) - (kmvmax*q[1]) - (kevmax*q[1])
     return [dpdt,dsdt]
 # Definindo valores iniciais
-p0 = 10
+p0 = 20
 s0 = 0   
 q0 = [p0, s0]
 # Definindo intervalo de tempo
-T = np.arange(0,15,0.0001)
+T = np.arange(0,10,0.0001)
 # Definindo odeint
 q = odeint(EquacoesDiferenciais, q0, T)
 # Gráfico THCp X Tempo
 plt.plot(T, q[:, 0])
-plt.ylabel('p(t)')
+plt.ylabel('[THC]')
 plt.xlabel('t')
 plt.title(r'Quantidade de THC no pulmão')
 plt.grid(True)
 plt.show()
 # Gráfico THCs X Tempo
 plt.plot(T, q[:, 1])
-plt.ylabel('s(t)')
+plt.xlim(0.1)
+plt.ylim(0.1)
+plt.ylabel('[THC]')
 plt.xlabel('t')
 plt.title(r'Quantidade de THC no sangue')
 plt.grid(True)
