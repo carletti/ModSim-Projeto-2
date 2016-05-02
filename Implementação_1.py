9fe146fb4d022415ffa37c41432c239d37cf5258
carletti <trcarletti@gmail.com>
2016-05-02 19:52:28 -0300

    Terminei os gráficos [THC] por tempo

--- Implementação_1.py
+++ Implementação_1.py
@@ -6,13 +6,13 @@ import numpy as np
 def EquacoesDiferenciais(q, t):
     ka = 0.95
     ks = 0.05
-    vmax = (13.6*(10**(-9))) # mol por minuto
+    vmax = (13.6*(10**(-11))) # mol por minuto
     km = (12.5*(10**(-9)*(314.45))) # Molaridade 
-    kevmax = 0.9
-    
+    kevmax = 0.9  
     dpdt = -(ka*q[0]) - (ks*q[0])
     dsdt = (ka*q[0]) - ((vmax*q[1])/(km + q[1])) - (kevmax*q[1])
     return [dpdt,dsdt]
+    
 # Definindo valores iniciais (1g de maconha)
 I0 = 0.1 # Gramas
 CI0 = I0/(5*1000) # Gramas por ml
@@ -23,6 +23,7 @@ q0 = [p0, s0]
 T = np.arange(0,10,0.0001) # Dias
 # Definindo odeint
 q1 = odeint(EquacoesDiferenciais, q0, T)
+
 # Definindo valores iniciais (2g de maconha)
 I0 = 0.2 # Gramas
 CI0 = I0/(5*1000) # Gramas por ml
@@ -33,15 +34,12 @@ q0 = [p0, s0]
 T = np.arange(0,10,0.0001) # Dias
 # Definindo odeint
 q2 = odeint(EquacoesDiferenciais, q0, T)
-# Quantidade limite no teste 
+
+# Quantidade limite no teste para urina
 limite= [] 
 for i in range(100000):
     l = 25*(10**(-9))
     limite.append(l)
-print(len(T))
-
-# Gráficos
-
 # Gráfico [THC]p X Tempo
 plt.plot(T, q1[:, 0])
 plt.plot(T, q2[:, 0])
@@ -61,3 +59,31 @@ plt.xlabel('Tempo (dias)')
 plt.title(r'Concentração de THC no sangue')
 plt.grid(True)
 plt.show()
+
+# Quantidade limite no teste para saliva
+limite= [] 
+for i in range(100000):
+    l = 50*(10**(-9))
+    limite.append(l)
+# Gráfico [THC]p X Tempo
+plt.plot(T, q1[:, 0])
+plt.plot(T, q2[:, 0])
+#plt.plot(T, q3[:, 0])
+plt.ylabel('[THC]')
+plt.xlabel('Tempo (dias)')
+plt.title(r'Concentração de THC no pulmão')
+plt.grid(True)
+plt.show()
+# Gráfico [THC]s X Tempo
+plt.plot(T, q1[:, 1])
+plt.plot(T, q2[:, 1])
+#plt.plot(T, q3[:, 1])
+plt.plot(T, limite)
+plt.ylabel('[THC]')
+plt.xlabel('Tempo (dias)')
+plt.title(r'Concentração de THC no sangue')
+plt.grid(True)
+plt.show()
+
+# Figuras  de mérito
+
