0fcc218cb9a03262dde80e1ea9b9d912fde577c6
carletti <trcarletti@gmail.com>
2016-05-02 17:01:29 -0300

    Arrumei os gráficos e as equações

--- Implementação_1.py
+++ Implementação_1.py
@@ -4,35 +4,45 @@ import matplotlib.pyplot as plt
 import numpy as np
 # Definindo equações
 def EquacoesDiferenciais(q, t):
-    ka = (95/100)
-    ks = (5/100)
-    kmvmax = (10/100) # Concentrção por segundo 
+    ka = 0.95
+    ks = 0.05
+    vmax = 0.1
+    km = 1 
     kevmax = 0.9
     
     dpdt = -(ka*q[0]) - (ks*q[0])
-    dsdt = (ka*q[0]) - (kmvmax*q[1]) - (kevmax*q[1])
+    dsdt = (ka*q[0]) - ((vmax*q[1])/(km + q[1])) - (kevmax*q[1])
     return [dpdt,dsdt]
-# Definindo valores iniciais
-p0 = 20
+# Definindo valores iniciais (1g de maconha)
+I0 = 1
+p0 = (0.1*I0)
 s0 = 0   
 q0 = [p0, s0]
 # Definindo intervalo de tempo
 T = np.arange(0,10,0.0001)
 # Definindo odeint
-q = odeint(EquacoesDiferenciais, q0, T)
+q1 = odeint(EquacoesDiferenciais, q0, T)
+# Definindo valores iniciais (2g de maconha)
+I0 = 2
+p0 = 0.1*I0
+s0 = 0   
+q0 = [p0, s0]
+# Definindo intervalo de tempo
+T = np.arange(0,10,0.0001)
+# Definindo odeint
+q2 = odeint(EquacoesDiferenciais, q0, T)
+
 # Gráfico THCp X Tempo
-plt.plot(T, q[:, 0])
+plt.plot(T, q1[:, 0])
 plt.ylabel('[THC]')
-plt.xlabel('t')
-plt.title(r'Quantidade de THC no pulmão')
+plt.xlabel('Tempo (dias)')
+plt.title(r'Concentração de THC no pulmão')
 plt.grid(True)
 plt.show()
 # Gráfico THCs X Tempo
-plt.plot(T, q[:, 1])
-plt.xlim(0.1)
-plt.ylim(0.1)
+plt.plot(T, q1[:, 1])
 plt.ylabel('[THC]')
-plt.xlabel('t')
-plt.title(r'Quantidade de THC no sangue')
+plt.xlabel('Tempo (dias)')
+plt.title(r'Concentração de THC no sangue')
 plt.grid(True)
 plt.show()
